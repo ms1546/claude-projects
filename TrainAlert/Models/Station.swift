cd07aa3d76780f6d19830aefd4c430d1074a2224
ms <maemotosato@gmail.coms>
2025-08-06 22:03:06 +0900

    fix

---
+++ TrainAlert/Models/Station.swift
@@ -0,0 +1,25 @@
+//
+//  Station.swift
+//  TrainAlert
+//
+//  Created by Claude on 2024/01/08.
+//
+
+import Foundation
+import CoreLocation
+
+struct Station: Identifiable, Codable, Equatable {
+    let id: String
+    let name: String
+    let latitude: Double
+    let longitude: Double
+    let lines: [String]
+    
+    var coordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+    
+    var location: CLLocation {
+        CLLocation(latitude: latitude, longitude: longitude)
+    }
+}
